92929881f700a67aaf81a30b85fb750b62285bea
Léo Massy <26872181+leomam@users.noreply.github.com>
2022-02-15 16:44:21 +0100

    Jeux rapide entre amis

---
+++ jeu/Au_plus_proche/question.js
@@ -0,0 +1,52 @@
+let tabquestionrep =    [["Combien d'États membres dans l'Union Européenne (2022) ?", "L'Union européenne (UE) compte 27 États membres en 2022"],
+                        ["Combien de grands fleuves en France ?", "5 : le Rhin, le Rhône, la Loire, la Seine, la Garonne"],
+                        ["Quel est la taille du Burj Khalifa en mètres ?", "828 m"],
+                        ["Quel est la taille de la Statue de la Liberté SANS socle en mètres ?", "46 m"],
+                        ["Quel est la taille de la Statue de la Liberté AVEC socle en mètres ?", "93 m"],
+                        ["Combien vaut 1 Mile en mètres ?", "1609 m"],
+                        ["Combien vaut la superficie de la Ruissie en km² ?", "17,13 millions km²"],
+                        ["Combien vaut la superficie de la France en km² ?", "543 940 km²"],
+                        ["Combien vaut la superficie de l'Espagne en km² ?", "505 990 km²"],
+                        ["Combien vaut la superficie de l'Allemagne en km² ?", "357 386 km²"],
+                        ["Combien vaut la distance entre Brest et New York à vol d'oiseau ?", "5383 m"],
+                        ["Combien d'arrondissements à Paris ?", "20"],
+                        ["Quel est la taille de la Tour Montparnasse en mètres ?", "209 m"]];
+
+
+function endGame() {
+    question = "FIN"
+    reponse = "Il n'y a plus de question dans le jeu, veuillez recommencer le jeu"
+	return [question, reponse]
+}
+
+
+function putquestionrep(question, reponse) {
+	document.getElementById("question").innerText = question;
+    document.getElementById("reponse").innerText = reponse;
+}
+
+function getRandomQuestion(tabquestionrep) {
+    if (tabquestionrep.length != 0) {
+        index = Math.floor(Math.random() * tabquestionrep.length)
+        item = tabquestionrep[index];
+        tabquestionrep.splice(index, 1)
+    } else {
+        item = endGame()
+        document.getElementById("nextbutton").innerText = "Recommencer";
+        document.getElementById("nextbutton").href = "index.html";
+        document.getElementById("nextbutton").removeAttribute("onclick");
+        document.getElementById("nextbutton").removeAttribute("data-bs-toggle");
+    }
+	return item;
+}
+
+function generateQuestion() {
+    document.getElementById("collapseExample").style = "visibility: hidden;";
+	questionrep = getRandomQuestion(tabquestionrep);
+	putquestionrep(questionrep[0], questionrep[1]);
+    setTimeout(function(){
+        document.getElementById("collapseExample").style = "visibility: visible;";
+    }, 400);
+}
+
+generateQuestion();
