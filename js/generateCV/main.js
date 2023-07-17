9780e0ee9f8b4f711262ea69faabde3592f2d963
leomam <26872181+leomam@users.noreply.github.com>
2023-07-17 15:11:10 +0200

    Update CV

--- js/generateCV/main.js
+++ js/generateCV/main.js
@@ -19,25 +19,55 @@ function generateCV(theme, langue){
     if (langue){
         //English
         var textSaveLang = "EN"
+        var textTitle = "Looking for work in Cybersecurity"
         var textBio = { age: "years old", drivelicence: "Driving licence", num: "+33 6 XX XX XX XX", location: "49300 Cholet (France)" }
         var textSkill = { header: "TECHNICAL\nSKILLS", progHeader: "Programming", secHeader: "Security", sysHeader: "System and networks", webHeader: "Web Front and Back", databaseHeader: "Database", projManage: "Project management", bigData: "Big Data", gameEngi: "Game engine" } 
         var textLang = { header: "FOREIGN\nLANGUAGES SKILLS", headerHighCorrection: -3.5, enLang: "English : Good working knowledge", esLang: "Spanish : Basic" }
-        var textHobbies = { header: "HOBBIES", climb: "Climbing : In an alpine club", bad: "Badminton: 3 years of application", ski: "Skiing : Every years"}
-        var textTitle = "Looking for work in Cybersecurity"
-        var textEduc = { header: "Education and Qualifications", today: "2022 : Digital Security Expert training", todaysub: "ENI Informatique (France)", past1: "2020 : Bachelor in computer science", past1sub: "Faculty of Sciences (France)", past2: "2019 : A 2 years Dipl. T in Computer Science", past2sub: "IUT of Lannion (France)" }
-        var textProj = { header: "Projects", firstProj: "C++ Project | 2020 - Team Work", firstProjsub: "Character syntax analysis to\ntrigger the desired functions.\nTools : Yacc and Lex", secondProj: "Web Project | 2019 - Team Work", secondProjsub: "PHP and PostgreSQL project with a\npersonal web pokédex for each user.\nTools : VS Code and CodeIgniter", thirdProj: "Web Project | 2018 - Team Work", thirdProjsub: "Website based on augmented reality.\nhttp://temangowebsite.gitlab.io/demo/\nTools : Brackets / Atom", fourthProj: "Java Project | 2017 - Team Work", fourthProjsub: "Based on the Travelling salesman problem.\nOptimization of working time of the Mailmen.\nTools : Eclipse / Git / Java / JavaFX", fifthProj: "Web Project | 2017 - Team Work", fifthProjsub: "Website based on quantum computers.\nTools : Brackets / HTML / CSS", sixthProj: "Android and PC Project | 2016 - Team Work", sixthProjsub: "Puzzle Game Android based on Java.\nTools : Java / Processing" }
-        var textWork = { header: "Work Experience", firstWork: "Product security at Saagie | 2020/2021 -\nWork-Study", firstWorksub: "Pair work, organization with Jira tickets,\nIncrease in IT security skills.\nSaagie (France)", secondWork: "Unity 3D Developer | Summer 2019 - Internship", secondWorksub: "Team work, organization with Trello,\nlinguistic improvement.\nUlster University (Northern Ireland)", thirdWork: "Logistics Officer | 2016 and 2017 - Summer Job", thirdWorksub: "Organized, ability to follow orders, team\nwork, Optimization of working time.\nCooperative bookshop \"la Sadel\" (France)" }
+        var textHobbies = { header: "HOBBIES", climb: "Climbing : 3 years of application", bad: "Badminton: 3 years of application", ski: "Skiing : Every years"}
+        
+        var textEduc = {    header: "Education and Qualifications",
+                            today: "2022 : Digital Security Expert training", todaysub: "ENI Informatique (France)",
+                            past1: "2020 : Bachelor in computer science", past1sub: "Faculty of Sciences (France)",
+                            past2: "2019 : A 2 years Dipl. T in Computer Science", past2sub: "IUT of Lannion (France)" }
+        
+        var textProj = {    header: "Projects",
+                            firstProj: "C++ Project | 2020 - Team Work", firstProjsub: "Character syntax analysis to\ntrigger the desired functions.\nTools : Yacc and Lex", 
+                            secondProj: "Web Project | 2019 - Team Work", secondProjsub: "PHP and PostgreSQL project with a\npersonal web pokédex for each user.\nTools : VS Code and CodeIgniter",
+                            thirdProj: "Web Project | 2018 - Team Work", thirdProjsub: "Website based on augmented reality.\nhttp://temangowebsite.gitlab.io/demo/\nTools : Brackets / Atom",
+                            fourthProj: "Java Project | 2017 - Team Work", fourthProjsub: "Based on the Travelling salesman problem.\nOptimization of working time of the Mailmen.\nTools : Eclipse / Git / Java / JavaFX",
+                            fifthProj: "Web Project | 2017 - Team Work", fifthProjsub: "Website based on quantum computers.\nTools : Brackets / HTML / CSS",
+                            sixthProj: "Android and PC Project | 2016 - Team Work", sixthProjsub: "Puzzle Game Android based on Java.\nTools : Java / Processing" }
+        
+        var textWork = {    header: "Work Experience",
+                            firstWork: "Product security at Saagie | 2020/2021 -\nWork-Study", firstWorksub: "Pair work, organization with Jira tickets,\nIncrease in IT security skills.\nSaagie (France)",
+                            secondWork: "Unity 3D Developer | Summer 2019 - Internship", secondWorksub: "Team work, organization with Trello,\nlinguistic improvement.\nUlster University (Northern Ireland)",
+                            thirdWork: "Logistics Officer | 2016 and 2017 - Summer Job", thirdWorksub: "Organized, ability to follow orders, team\nwork, Optimization of working time.\nCooperative bookshop \"la Sadel\" (France)" }
     } else {
         //French
         var textSaveLang = "FR"
-        var textBio = { age: "ans", drivelicence: "Permis B", num: "06 XX XX XX XX", location: "49300 Cholet" }
+        var textTitle = "Administration Cybersécurité"
+        var textBio = { age: "ans", drivelicence: "Permis B", num: "06 XX XX XX XX", location: "85290 Mortagne-sur-sèvre" }
         var textSkill = { header: "COMPÉTENCES\nPROFESSIONNELLES", progHeader: "Programmation", secHeader: "Securité", sysHeader: "Systèmes et Réseaux", webHeader: "Web Front et Back", databaseHeader: "Bases de données", projManage: "Gestion de projets", bigData: "Big Data", gameEngi: "Moteur de Jeux" }
         var textLang = { header: "LANGUES", headerHighCorrection: 0, enLang: "Anglais : Niveau C1", esLang: "Espagnol : Niveau B1" }
-        var textHobbies = { header: "LOISIRS", climb: "Escalade : En club", bad: "Badminton : 3 ans", ski: "Ski : tous les ans" }
-        var textTitle = "Recherche CDI Cybersécurité"
-        var textEduc = { header: "Formation", today: "2022 : Formation Expert en Sécurité Digitale", todaysub: "ENI École Informatique (44)", past1: "2020 : Licence Informatique", past1sub: "Faculté des Sciences (49)", past2: "2019 : DUT Informatique", past2sub: "IUT de Lannion (22)"}
-        var textProj = { header: "Projets", firstProj: "Projet C++, Yacc et Lex | 2020 - Équipe", firstProjsub: "Analyses syntaxiques de caractères pour\ndéclencher les fonctions voulues.\nOutils : Yacc et Lex", secondProj: "Projet Web PHP | 2019 - Équipe", secondProjsub: "Site Projet PHP et PostgreSQL pour\nréaliser un pokédex personnel.\nOutils : VS Code et CodeIgniter", thirdProj: "Projet Web | 2018 - Équipe", thirdProjsub: "Site Web vitrine sur la réalité augmentée.\nhttp://temangowebsite.gitlab.io/demo/\nOutils : Brackets / Atom", fourthProj: "Projet Java | 2017 - Équipe", fourthProjsub: "Application Java basée sur le\nproblème du voyageur de commerce.\nOutils : Eclipse / Git / Java / JavaFX", fifthProj: "Projet Web | 2017 - Équipe", fifthProjsub: "Site vitrine sur les ordinateurs quantiques.\nOutils : Brackets / HTML / CSS", sixthProj: "Projet Android et PC | 2016 - Équipe", sixthProjsub: "Jeu de réflexion Android basé sur Java.\nOutils : Java / Processing" }
-        var textWork = { header: "Expérience Professionnelle", firstWork: "Sécurité produit chez Meteodyn | 2021/2022 -\nAlternance", firstWorksub: "Travail en équipe, organisation avec tickets Git,\nMontée en compétences sur l'aspect sécurité.\nMeteodyn (Saint-Herblain)", secondWork: "Sécurité produit chez Saagie | 2020/2021 -\nAlternance", secondWorksub: "Travail en binôme, organisation avec tickets Jira,\nMontée en compétences sur l'aspect sécurité.\nSaagie (Rouen)", thirdWork: "Développeur Unity 3D | Été 2019 - Stage", thirdWorksub: "Travail en équipe, amélioration linguistique.\nUlster University (Irlande du Nord)" }
+        var textHobbies = { header: "LOISIRS", climb: "Escalade : 3 ans", bad: "Badminton : 3 ans", ski: "Ski : tous les ans" }
+        
+        var textEduc = {    header: "Formation",
+                            today: "2022 : Formation Expert en Sécurité Digitale", todaysub: "ENI École Informatique (44)",
+                            past1: "2020 : Licence Informatique", past1sub: "Faculté des Sciences (49)",
+                            past2: "2019 : DUT Informatique", past2sub: "IUT de Lannion (22)"}
+        
+        var textProj = {    header: "Projets",
+                            firstProj: "Projet ElasticSearch, Logs | 2022 - Autonomie", firstProjsub: "Mise en place d'un puit de logs pour retracer\nles événements du parc informatique de l'entreprise.\nOutils : Winlogbeat, Filebeat",
+                            secondProj: "Projet Firewalling | 2022 - Autonomie", secondProjsub: "Mise en place des règles de par feu de\nl'entreprise pour isoler les flux réseau.\nOutil : Fortigate",
+                            thirdProj: "Projet VLAN | 2022 - Équipe", thirdProjsub: "Implémentation des VLAN dans l'entreprise.",
+                            fourthProj: "Projet MAJ sécurité | 2021 - Autonomie", fourthProjsub: "Mise en place de routine de mise à jour des\nserveurs exposés à internet et hardening.",
+                            fifthProj: "Projet Web PHP | 2019 - Équipe", fifthProjsub: "Site Projet PHP et PostgreSQL pour\nréaliser un pokédex personnel.\nOutils : VS Code et CodeIgniter",
+                            sixthProj: "Projet Java | 2017 - Équipe", sixthProjsub: "Application Java basée sur le\nproblème du voyageur de commerce.\nOutils : Eclipse, Git, Java et JavaFX" }
+        
+        var textWork = {    header: "Expérience Professionnelle",
+                            firstWork: "Sécurité produit chez Meteodyn | 2021/2022 -\nAlternance", firstWorksub: "Travail en équipe, organisation avec tickets Git,\nMontée en compétences sur l'aspect sécurité.\nMeteodyn (Saint-Herblain)",
+                            secondWork: "Sécurité produit chez Saagie | 2020/2021 -\nAlternance", secondWorksub: "Travail en binôme, organisation avec tickets Jira,\nMontée en compétences sur l'aspect sécurité.\nSaagie (Rouen)",
+                            thirdWork: "Développeur Unity 3D | Été 2019 - Stage", thirdWorksub: "Travail en équipe, amélioration linguistique.\nUlster University (Irlande du Nord)" }
     }
 
     //Profil Photo
@@ -129,7 +159,7 @@ function generateCV(theme, langue){
     doc.setFontSize(14);
     doc.text(textSkill.databaseHeader, 45, 187 + lcategory4h, 'center');
     doc.setFontSize(8);
-    doc.text("PostgreSQL / MongoDB / Tutorial D", 45, 190.5  + lcategory4h, 'center');
+    doc.text("MySQL / PostgreSQL / MongoDB / Tutorial D", 45, 190.5  + lcategory4h, 'center');
     doc.setFontSize(14);
     doc.text(textSkill.projManage, 45, 196 + lcategory4h, 'center');
     doc.setFontSize(8);
@@ -141,7 +171,7 @@ function generateCV(theme, langue){
     doc.setFontSize(14);
     doc.text(textSkill.gameEngi, 45, 214 + lcategory4h, 'center');
     doc.setFontSize(8);
-    doc.text("Unity3D", 45, 217.5  + lcategory4h, 'center');
+    doc.text("Unity3D / Unreal Engine", 45, 217.5  + lcategory4h, 'center');
 
     //LCategory 5
     let lcategory5h = 0
@@ -182,104 +212,128 @@ function generateCV(theme, langue){
     doc.text(textTitle, 150, 8, 'center');
 
     //RCategory 1
-    let rcategory1h = 0
+    let rcategory1h = 248 // Hauteur de la catégorie
     doc.setFontSize(16);
     doc.setTextColor(0, 0, 0);
-    doc.text(textEduc.header, 95, 19 + rcategory1h);
-    backgroundLine(21 + rcategory1h, secondaryColor);
+    doc.text(textEduc.header, 95, rcategory1h);
+    backgroundLine(2 + rcategory1h, secondaryColor);
     doc.setFontSize(14);
-    doc.text(textEduc.today, 100, 29 + rcategory1h);
+    rcategory1h += 10;
+    doc.text(textEduc.today, 100, rcategory1h);
     doc.setTextColor(secondaryColor[0]/2,secondaryColor[1]/2,secondaryColor[2]/2);
     doc.setFontSize(12);
-    doc.text(textEduc.todaysub, 205, 34 + rcategory1h, 'right');
+    rcategory1h += 5;
+    doc.text(textEduc.todaysub, 205, rcategory1h, 'right');
     doc.setTextColor(0, 0, 0);
     doc.setFontSize(14);
-    doc.text(textEduc.past1, 100, 43 + rcategory1h);
+    rcategory1h += 10;
+    doc.text(textEduc.past1, 100, rcategory1h);
     doc.setTextColor(secondaryColor[0]/2,secondaryColor[1]/2,secondaryColor[2]/2);
     doc.setFontSize(12);
-    doc.text(textEduc.past1sub, 205, 48 + rcategory1h, 'right');
+    rcategory1h += 5;
+    doc.text(textEduc.past1sub, 205, rcategory1h, 'right');
     doc.setTextColor(0, 0, 0);
     doc.setFontSize(14);
-    doc.text(textEduc.past2, 100, 57 + rcategory1h);
+    rcategory1h += 10;
+    doc.text(textEduc.past2, 100, rcategory1h);
     doc.setTextColor(secondaryColor[0]/2,secondaryColor[1]/2,secondaryColor[2]/2);
     doc.setFontSize(12);
-    doc.text(textEduc.past2sub, 205, 62 + rcategory1h, 'right');
+    rcategory1h += 5;
+    doc.text(textEduc.past2sub, 205, rcategory1h, 'right');
 
     //RCategory 2
-    let rcategory2h = -10
+    let rcategory2h = 103 // Hauteur de la catégorie
     doc.setFontSize(16);
     doc.setTextColor(0, 0, 0);
-    doc.text(textProj.header, 95, 78 + rcategory2h);
-    backgroundLine(80 + rcategory2h, secondaryColor);
+    doc.text(textProj.header, 95, rcategory2h);
+    backgroundLine(2 + rcategory2h, secondaryColor);
     doc.setFontSize(14);
-    doc.text(textProj.firstProj, 100, 88 + rcategory2h);
+    rcategory2h += 10;
+    doc.text(textProj.firstProj, 100, rcategory2h);
     doc.setTextColor(secondaryColor[0]/2,secondaryColor[1]/2,secondaryColor[2]/2);
     doc.setFontSize(12);
-    doc.text(textProj.firstProjsub, 105, 94 + rcategory2h);
+    rcategory2h += 6;
+    doc.text(textProj.firstProjsub, 105, rcategory2h);
 
     doc.setTextColor(0, 0, 0);
     doc.setFontSize(14);
-    doc.text(textProj.secondProj, 100, 113 + rcategory2h);
+    rcategory2h += 18;
+    doc.text(textProj.secondProj, 100, rcategory2h);
     doc.setTextColor(secondaryColor[0]/2,secondaryColor[1]/2,secondaryColor[2]/2);
     doc.setFontSize(12);
-    doc.text(textProj.secondProjsub, 105, 119 + rcategory2h);
+    rcategory2h += 6;
+    doc.text(textProj.secondProjsub, 105, rcategory2h);
 
     doc.setTextColor(0, 0, 0);
     doc.setFontSize(14);
-    doc.text(textProj.thirdProj, 100, 138 + rcategory2h);
+    rcategory2h += 18;
+    doc.text(textProj.thirdProj, 100, rcategory2h);
     doc.setTextColor(secondaryColor[0]/2,secondaryColor[1]/2,secondaryColor[2]/2);
     doc.setFontSize(12);
-    doc.text(textProj.thirdProjsub, 105, 144 + rcategory2h);
+    rcategory2h += 6;
+    doc.text(textProj.thirdProjsub, 105, rcategory2h);
 
     doc.setTextColor(0, 0, 0);
     doc.setFontSize(14);
-    doc.text(textProj.fourthProj, 100, 163 + rcategory2h);
+    rcategory2h += 10;
+    doc.text(textProj.fourthProj, 100, rcategory2h);
     doc.setTextColor(secondaryColor[0]/2,secondaryColor[1]/2,secondaryColor[2]/2);
     doc.setFontSize(12);
-    doc.text(textProj.fourthProjsub, 105, 169 + rcategory2h);
+    rcategory2h += 6;
+    doc.text(textProj.fourthProjsub, 105, rcategory2h);
 
     doc.setTextColor(0, 0, 0);
     doc.setFontSize(14);
-    doc.text(textProj.fifthProj, 100, 188 + rcategory2h);
+    rcategory2h += 14;
+    doc.text(textProj.fifthProj, 100, rcategory2h);
     doc.setTextColor(secondaryColor[0]/2,secondaryColor[1]/2,secondaryColor[2]/2);
     doc.setFontSize(12);
-    doc.text(textProj.fifthProjsub, 105, 194 + rcategory2h);
+    rcategory2h += 6;
+    doc.text(textProj.fifthProjsub, 105, rcategory2h);
 
     doc.setTextColor(0, 0, 0);
     doc.setFontSize(14);
-    doc.text(textProj.sixthProj, 100, 207 + rcategory2h);
+    rcategory2h += 18;
+    doc.text(textProj.sixthProj, 100, rcategory2h);
     doc.setTextColor(secondaryColor[0]/2,secondaryColor[1]/2,secondaryColor[2]/2);
     doc.setFontSize(12);
-    doc.text(textProj.sixthProjsub, 105, 213 + rcategory2h);
+    rcategory2h += 6;
+    doc.text(textProj.sixthProjsub, 105, rcategory2h);
 
 
     //RCategory 3 - Expérience Professionnelle
-    let rcategory3h = -14
+    let rcategory3h = 19 // Hauteur de la catégorie
     doc.setFontSize(16);
     doc.setTextColor(0, 0, 0);
-    doc.text(textWork.header, 95, 232 + rcategory3h);
-    backgroundLine(234 + rcategory3h, secondaryColor);
+    doc.text(textWork.header, 95, rcategory3h);
+    backgroundLine(2 + rcategory3h, secondaryColor);
 
     doc.setTextColor(0, 0, 0);
     doc.setFontSize(14);
-    doc.text(textWork.firstWork, 100, 242 + rcategory3h);
+    rcategory3h += 10;
+    doc.text(textWork.firstWork, 100, rcategory3h);
     doc.setTextColor(secondaryColor[0]/2,secondaryColor[1]/2,secondaryColor[2]/2);
     doc.setFontSize(12);
-    doc.text(textWork.firstWorksub, 105, 253 + rcategory3h);
+    rcategory3h += 10;
+    doc.text(textWork.firstWorksub, 105, rcategory3h);
 
     doc.setTextColor(0, 0, 0);
     doc.setFontSize(14);
-    doc.text(textWork.secondWork, 100, 270 + rcategory3h);
+    rcategory3h += 17;
+    doc.text(textWork.secondWork, 100, rcategory3h);
     doc.setTextColor(secondaryColor[0]/2,secondaryColor[1]/2,secondaryColor[2]/2);
     doc.setFontSize(12);
-    doc.text(textWork.secondWorksub, 105, 281 + rcategory3h);
+    rcategory3h += 10;
+    doc.text(textWork.secondWorksub, 105, rcategory3h);
 
     doc.setTextColor(0, 0, 0);
     doc.setFontSize(14);
-    doc.text(textWork.thirdWork, 100, 298 + rcategory3h);
+    rcategory3h += 17;
+    doc.text(textWork.thirdWork, 100, rcategory3h);
     doc.setTextColor(secondaryColor[0]/2,secondaryColor[1]/2,secondaryColor[2]/2);
     doc.setFontSize(12);
-    doc.text(textWork.thirdWorksub, 105, 303 + rcategory3h);
+    rcategory3h += 5;
+    doc.text(textWork.thirdWorksub, 105, rcategory3h);
 
     //Save A4 Format
     //210 x 297
